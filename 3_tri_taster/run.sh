7457ea72dd42969b225b064d7619021b9cd95882
lrc <ruichen.fatd@gmail.com>
2020-12-03 01:08:28 +0800

    modify run.sh

--- 3_tri_taster/run.sh
+++ 3_tri_taster/run.sh
@@ -1,4 +1,15 @@
+#!/usr/local/bin/zsh
+set -e
+if [ ! -d build ]; then
+    mkdir build
+fi
+
 cd build
+if [ ! -f CMakeCache.txt ]; then
+    cmake ..
+fi
+
 cmake --build .
 ./RenderTutorial
+open .
 cd -
